a77817077c76c10b70a63d33ec8f8e816d3c7e03
azulios-studio <96632377+azulios-studio@users.noreply.github.com>
2021-12-24 19:35:21 +0100

    Add files via upload

---
+++ main-main.js
@@ -0,0 +1,7 @@
+alert("Veuillez cliquer sur ok pour accepter nos conditions")
+
+let string = "L'utilisateur à cliqué sur ok"
+let warning = ("Si vous ne savez ce que vous faites ici veuillez gentillement fermer cette fenêtre")
+
+console.log(string);
+console.warn(warning);
